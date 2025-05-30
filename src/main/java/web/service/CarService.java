89705534b6efaddfd0c5d7a3632baee51592bf35
Arina M <arenamaralena@yandex.ru>
2025-05-30 22:07:46 +0300

    fix CarService,CarServiceImpl

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -5,5 +5,5 @@ import web.model.Car;
 import java.util.List;
 
 public interface CarService {
-    public List<Car> getSomeCars(int amount);
+    List<Car> getSomeCars(int amount);
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,12 +1,13 @@
 package web.service;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+@Service
 public class CarServiceImpl implements CarService {
     private static int START_OF_LIST = 0;
     private List<Car> cars;
